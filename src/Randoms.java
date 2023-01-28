15120b4b666c1826e29b8495321dab715146b097
Rainir <off_rainir@proton.me>
2023-01-28 21:36:34 +0300

    First commit

---
+++ src/Randoms.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.Iterator;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random = new Random();
+    List<Integer> numbs;
+//    int min;
+//    int max;
+    public Randoms(int min, int max) {
+         numbs = random.ints(5, min, max + 1).boxed().toList();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            int nextNum = 0;
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                int numb;
+                if (nextNum == numbs.size()) {
+                    //List<Integer> numbs = random.ints(10, min, max).boxed().toList();
+                    numb = numbs.get(0);
+                    nextNum = 1;
+                } else {
+                    numb = numbs.get(nextNum);
+                    nextNum++;
+                }
+                return numb;
+            }
+        };
+    }
+}
